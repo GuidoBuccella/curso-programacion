3352ec946b66c6cbc871f36fba14090a5ec3ecb2
guidobuccella <guidobuccella@hotmail.com>
2022-08-29 18:19:38 -0300

    variables,constructores

---
+++ src/retak/AppRetak.java
@@ -0,0 +1,6 @@
+package retak;
+
+public class AppRetak {
+
+
+}
---
+++ src/retak/TipoRetak.java
@@ -0,0 +1,16 @@
+package retak;
+
+public enum TipoRetak {
+
+    LADRILLO,
+    LADRILLO_REFUERZO,
+    PLACA_AISLACION,
+    ESCALON,
+    DINTEL,
+    BOLSAS_ALBANIL,
+    REVOQUES,
+    MOLDURAS,
+    HERRAMIENTAS
+
+
+}
